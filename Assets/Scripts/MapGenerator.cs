0be9e33570b71844689f1988a214e1648674c9e1
VanBronckhorst <filippo.pellolio@mail.polimi.it>
2022-01-02 18:39:29 -0800

    E15.1

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -138,6 +138,8 @@ public class MapGenerator : MonoBehaviour {
             }
         }
 
+        textureData.UpdateMeshHeights(terrainMaterial, terrainData.minHeight, terrainData.maxHeight);
+
         return new MapData(noiseMap);
 
     }
